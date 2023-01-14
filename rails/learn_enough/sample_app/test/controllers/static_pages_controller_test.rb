438f03aaaa280dfbabe02f473c0584edd0353206
Fernando Batista <fernandohsbatista@gmail.com>
2023-01-14 13:17:05 -0300

    rails: (learn enough) - chapter 3 - sample_app - refactor static pages titles by using the application view template

--- rails/learn_enough/sample_app/test/controllers/static_pages_controller_test.rb
+++ rails/learn_enough/sample_app/test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Home page of my sample app"
+    assert_select "title", "Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Help page of my sample app"
+    assert_select "title", "Help"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | About page of my sample app"
+    assert_select "title", "About"
   end
 end
