2b64ba4efe04d8839b011643182858fbd5dfbae4
Fernando Batista <fernando.sbatista@dimensa.com.br>
2023-04-10 17:58:41 -0300

    swift: (exercises) - hello world

---
+++ swift/Exercises/Exercises/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Exercises
+//
+//  Created by Fernando batista on 10/04/23.
+//
+
+import Foundation
+
+func printMessage() {
+    print("Hello world!!!")
+}
+
+printMessage()
+
