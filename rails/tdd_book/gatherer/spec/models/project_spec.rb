0ef102a10866ee59e1133c6c4eaab8204026f43a
Fernando Batista <fernandohsbatista@gmail.com>
2023-04-29 20:59:02 -0300

    rails: (tdd book) - gatherer project - refactoring tests to use "let"

--- rails/tdd_book/gatherer/spec/models/project_spec.rb
+++ rails/tdd_book/gatherer/spec/models/project_spec.rb
@@ -1,15 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Project do
+  let(:project) { Project.new }
+  let(:task) { Task.new }
   it "considers a project with tasks to be done" do
-    project = Project.new
     expect(project.done?).to be_truthy
   end
 
-  it "considers a project with an incomplete task is not done" do 
-    project = Project.new
-    task = Task.new
+  it "considers a project with an incomplete task is not done" do
     project.tasks << task
-    expect(project.done?)to be_falsy
+    expect(project.done?).to be_falsy
   end
 end
