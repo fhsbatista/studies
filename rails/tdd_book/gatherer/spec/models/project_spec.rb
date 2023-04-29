0d0a91f8fd637b60894a2745efd35a4ed250688e
Fernando Batista <fernandohsbatista@gmail.com>
2023-04-29 20:59:56 -0300

    rails: (tdd book) - gatherer project - refactoring tests to use "be_whatever" matcher

--- rails/tdd_book/gatherer/spec/models/project_spec.rb
+++ rails/tdd_book/gatherer/spec/models/project_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Project do
   let(:project) { Project.new }
   let(:task) { Task.new }
   it "considers a project with tasks to be done" do
-    expect(project.done?).to be_truthy
+    expect(project).to be_done
   end
 
   it "considers a project with an incomplete task is not done" do
     project.tasks << task
-    expect(project.done?).to be_falsy
+    expect(project).not_to be_done
   end
 end
