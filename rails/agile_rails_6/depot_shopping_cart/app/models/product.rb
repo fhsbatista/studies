7daef05a734ea1e3cfb6382c1a310b41ff694022
Fernando Batista <fernandohsbatista@gmail.com>
2023-05-11 20:42:31 -0300

    rails: (agile_rails_6) - depot project - set validations for product creation

--- rails/agile_rails_6/depot_shopping_cart/app/models/product.rb
+++ rails/agile_rails_6/depot_shopping_cart/app/models/product.rb
@@ -1,2 +1,10 @@
+require 'uri'
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, format: {
+    with: URI::DEFAULT_PARSER.make_regexp(%w[http https]),
+    message: 'must be a valid URL.'
+  }
 end
