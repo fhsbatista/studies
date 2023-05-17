e8d8a6eae427cb1f0299d0e82b16b38ef66fedd2
Fernando Batista <fernandohsbatista@gmail.com>
2023-05-17 20:26:06 -0300

    rails: (agile_rails_6) - depot project - add tests to "add to cart"

--- rails/agile_rails_6/depot_shopping_cart/test/controllers/line_items_controller_test.rb
+++ rails/agile_rails_6/depot_shopping_cart/test/controllers/line_items_controller_test.rb
@@ -17,8 +17,13 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference("LineItem.count") do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
+
+    follow_redirect!
+
+    assert_select 'h2', 'Depot cart'
+    assert_select 'li', 'Programming Ruby 1.0'
 
     assert_redirected_to line_item_url(LineItem.last)
   end
