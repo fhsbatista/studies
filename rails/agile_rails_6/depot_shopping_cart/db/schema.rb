48c2504b1598b09c7b178170b637f4982042c2da
Fernando Batista <fernandohsbatista@gmail.com>
2023-05-22 22:00:15 -0300

    rails: (agile_rails_6) - depot project - migration to combine line_items of the same product which quantity is 1

--- rails/agile_rails_6/depot_shopping_cart/db/schema.rb
+++ rails/agile_rails_6/depot_shopping_cart/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_004330) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_005445) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
