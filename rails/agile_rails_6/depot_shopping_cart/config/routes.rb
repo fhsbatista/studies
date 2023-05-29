7304b96bff6572b4d2fdf449b1cbd3be4f5bf4a4
Fernando Batista <fernandohsbatista@gmail.com>
2023-05-28 21:24:59 -0300

    rails: (agile_rails_6) - depot project - orders by product atom feature

--- rails/agile_rails_6/depot_shopping_cart/config/routes.rb
+++ rails/agile_rails_6/depot_shopping_cart/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
-  resources :products
   root "store#index", as: "store_index"
+  resources :products do
+    get :who_bought, on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
