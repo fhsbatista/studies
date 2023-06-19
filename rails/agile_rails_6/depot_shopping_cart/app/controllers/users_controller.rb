2bb18ef39f560d88be6205fa2ee2bf36618fe722
Fernando Batista <fernandohsbatista@gmail.com>
2023-06-19 20:58:38 -0300

    rails: (agile_rails_6) - depot project - avoid delete last user

--- rails/agile_rails_6/depot_shopping_cart/app/controllers/users_controller.rb
+++ rails/agile_rails_6/depot_shopping_cart/app/controllers/users_controller.rb
@@ -49,11 +49,15 @@ class UsersController < ApplicationController
 
   # DELETE /users/1 or /users/1.json
   def destroy
-    @user.destroy
+    begin
+      @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    rescue StandardError => exception
+      redirect_to users_url, notice: exception.message
     end
   end
 
