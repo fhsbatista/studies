f13458bb77f88458641da3155f9ece274a60d0e5
Fernando Batista <fernandohsbatista@gmail.com>
2024-09-27 20:08:41 -0300

    clojure: (cesar-alcancio) - hello world api

--- clojure/cesar_alcancio/clojure-service/src/clojure_service/core.clj
+++ clojure/cesar_alcancio/clojure-service/src/clojure_service/core.clj
@@ -1,6 +1,18 @@
-(ns clojure-service.core)
+(ns clojure-service.core
+  (:require [io.pedestal.http.route :as route]
+            [io.pedestal.http :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn hello-function [request]
+  {:status 200 :body (str "Hello world " (get-in request [:query-params :name] "everyone"))})
+
+(def routes (route/expand-routes
+              #{["/hello" :get hello-function :route-name :hello-world]}))
+
+(def service-map {::http/routes routes
+                  ::http/port   9999
+                  ::http/type   :jetty
+                  ::http/join?  false})
+
+(http/start (http/create-server service-map))
+(println "Started server http")
