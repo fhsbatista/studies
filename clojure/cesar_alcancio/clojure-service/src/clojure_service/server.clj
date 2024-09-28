5bae323f1c9e4af49ff531dfb1b250bdc33c0e8a
Fernando Batista <fernandohsbatista@gmail.com>
2024-09-27 21:26:10 -0300

    clojure: (cesar-alcancio) - task apis

--- clojure/cesar_alcancio/clojure-service/src/clojure_service/server.clj
+++ clojure/cesar_alcancio/clojure-service/src/clojure_service/server.clj
@@ -3,12 +3,31 @@
             [io.pedestal.http :as http]
             [io.pedestal.test :as test]))
 
+(def store (atom {}))
 
 (defn hello-function [request]
   {:status 200 :body (str "Hello world " (get-in request [:query-params :name] "everyone"))})
 
+(defn create-task-map [uuid name status]
+  {:id uuid :name name :status status})
+
+(defn create-task [request]
+  (let [uuid (java.util.UUID/randomUUID)
+        name (get-in request [:query-params :name])
+        status (get-in request [:query-params :status])
+        task (create-task-map uuid name status)]
+    (swap! store assoc uuid task)
+    {:status 200 :body {:message "Task created"
+                        :task    task}}
+    ))
+
+(defn list-tasks [request]
+  {:status 200 :body @store})
+
 (def routes (route/expand-routes
-              #{["/hello" :get hello-function :route-name :hello-world]}))
+              #{["/hello" :get hello-function :route-name :hello-world]
+                ["/task" :post create-task :route-name :create-task]
+                ["/list-tasks" :get list-tasks :route-name :list-tasks]}))
 
 (def service-map {::http/routes routes
                   ::http/port   9999
@@ -21,8 +40,11 @@
   (reset! server (http/stop (http/create-server service-map)))
   )
 
-(start-server)
 (defn test-request [verb url]
   (test/response-for (::http/service-fn @server) verb url))
 
+(start-server)
 (test-request :get "/hello?name=fernando")
+(test-request :post "/task?name=run&status=pending")
+
+(test-request :get "/list-tasks")
