5339325d53e5525cf6667c7739a610072a116e15
Fernando Batista <fernandohsbatista@gmail.com>
2024-10-20 10:51:05 -0300

    clojure: (clojure-for-the-brave) - syntax: str replace (chapter 3)

--- clojure/clojure-for-brave/src/clojure_for_brave/regex.clj
+++ clojure/clojure-for-brave/src/clojure_for_brave/regex.clj
@@ -1,12 +1,18 @@
-(ns clojure-for-brave.regex)
+(ns clojure-for-brave.regex
+  (:require [clojure.string :as str]))
 
 (defn find-and-print
   [finding-str str-found]
   (println (str "Searching for \"" finding-str "\": " str-found)))
+
+(defn find-and-replace
+  [text pattern new-text]
+  (str/replace text pattern new-text))
 
 (defn -main []
   (find-and-print "^left-" (re-find #"^left-" "left-eye")) ;returns left-
   (find-and-print "^left-" (re-find #"^left-" "right-eye")) ;returns nil
   (find-and-print "^left-" (re-find #"^left-" "cleft-eye")) ;returns nil
   (find-and-print "left-" (re-find #"left-" "cleft-eye")) ;returns left-
+  (println (find-and-replace "left eye" #"^left" "right" )) ;return "right eye"
   )
