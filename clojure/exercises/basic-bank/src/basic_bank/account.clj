889aec61b31dbe4bb58e85d4c0fa48b544e567ba
Fernando Batista <fernandohsbatista@gmail.com>
2024-10-22 20:28:04 -0300

    clojure: (exercises-basicbank) - refactor account to use json instead of txt

--- clojure/exercises/basic-bank/src/basic_bank/account.clj
+++ clojure/exercises/basic-bank/src/basic_bank/account.clj
@@ -1,18 +1,52 @@
-(ns basic-bank.account)
+(ns basic-bank.account
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io])
+  (:import [java.time LocalDateTime]
+           [java.time.format DateTimeFormatter]))
 
 (defn create-account [document name]
-  (let [filename (str  "accounts/" document ".txt")]
-    (spit filename (str "Owner document: " document))
-    (spit filename (str "\nOwner name: " name) :append true)
-    (spit filename (str "\nInitial balance: 0.0") :append true)
+  (let [filename (str  "accounts/" document ".json")
+        current-date (let [now (LocalDateTime/now)
+                           formatter (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss")]
+                       (.format now formatter))
+        balance 0.0
+        initial-data {:document document
+                      :name name
+                      :balance balance
+                      :created-at current-date
+                      :transactions []}
+        json-output (json/generate-string initial-data {:pretty true})]
+    (with-open [writer (io/writer filename)]
+      (.write writer json-output)
+      {:document document
+       :name name
+       :balance balance
+       :filename filename})))
+
+(defn get-account [document]
+  (let [filename (str "accounts/" document ".json")
+        content (with-open [reader (io/reader filename)]
+                  (json/parse-stream reader true))
+        name (content :name)
+        balance (content :balance)
+        created-at (content :created-at)]
     {:document document
      :name name
-     :balance 0.0
+     :created-at created-at
+     :balance balance
      :filename filename}))
 
 (defn deposit [account value]
-  (let [updated-balance (+ (account :balance) value)]
-    (spit (account :filename) (str "\nDeposit: " value " | Balance: " updated-balance) :append true)
-    (assoc account :balance updated-balance)))
-
-
+  (let [updated-balance (+ (account :balance) value)
+        current-date (let [now (LocalDateTime/now)
+                           formatter (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss")]
+                       (.format now formatter))
+        current-json (with-open [reader (io/reader (account :filename))]
+                       (json/parse-stream reader true))
+        deposit-json {:date current-date
+                      :value value}
+        updated-transactions (conj (current-json :transactions) deposit-json)
+        updated-json (assoc current-json :transactions updated-transactions
+                            :balance updated-balance)]
+    (with-open [writer (io/writer (account :filename))]
+      (.write writer (json/generate-string updated-json {:pretty true})))))
