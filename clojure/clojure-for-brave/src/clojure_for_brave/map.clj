8977ceb6c29d221a7a5a9ef68ae22682b209eab6
Fernando Batista <fernandohsbatista@gmail.com>
2024-10-28 10:09:04 -0300

    clojure: (clojure-for-the-brave) - core concepts: map - functions as params (chapter 4)

--- clojure/clojure-for-brave/src/clojure_for_brave/map.clj
+++ clojure/clojure-for-brave/src/clojure_for_brave/map.clj
@@ -12,8 +12,30 @@
     (doseq [item receipt]
       (println item))))
 
+
+(def sum #(reduce + %))
+
+(def avg #(/ (sum %) (count %)))
+
+(defn stats [numbers]
+  ;sum and avg are params being passed as functions
+  (map #(% numbers) [sum avg]))
+
+(def identities
+  [{:alias "Batman" :real "Bruce Wayne"}
+   {:alias "Spider-Man" :real "Peter Parker"}
+   {:alias "Santa" :real "Your mom"}
+   {:alias "Easter Bunny" :real "Your dad"}])
+
 (defn -main []
   (println (map inc [1 2 3])) ;=> [2 3 4]
   ;Passing multiple collections to map, all collections will be interated at once.
   (println (map + [1 2 3] [1 2 3])) ;=> [2 4 6] cuz (1+1 2+2 3+3)
-  (calc-sales))
+  (calc-sales)
+  (let [numbers [2 3 5 7 11]]
+    (println (str "Sum: " numbers " = "(sum numbers)))
+    (println (str "Avg: " numbers " = "(avg numbers)))
+    (println (str "Sum/Avg: " numbers " = "(stats numbers)))
+    )
+  ;map can be used to evaluate a key of items in a hash map
+  (println (map :real identities)))
