3ee35f575936bd468a8620bb836464dc68e1e166
Fernando Batista <fernandohsbatista@gmail.com>
2024-11-08 21:50:09 -0300

    datomic: (alura-ecommerce) - db functions to open connection

--- datomic/ecommerce/src/ecommerce/core.clj
+++ datomic/ecommerce/src/ecommerce/core.clj
@@ -1,6 +1,7 @@
-(ns ecommerce.core)
+(ns ecommerce.core
+  (:use clojure.pprint)
+  (:require [ecommerce.db :as db]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def conn (db/open-connection))
+(pprint conn)
+
---
+++ datomic/ecommerce/src/ecommerce/db.clj
@@ -0,0 +1,12 @@
+(ns ecommerce.db
+  (:use clojure.pprint)
+  (:require [datomic.api :as d]))
+
+(def db-uri "datomic:dev://localhost:4334/hello")
+
+(defn open-connection []
+  (d/create-database db-uri)
+  (d/connect db-uri))
+
+(defn delete-database []
+  (d/delete-database db-uri))
