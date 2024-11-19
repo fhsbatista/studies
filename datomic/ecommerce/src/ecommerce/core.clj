e0b0c523cf4e8b6caf3e627038dec2c6e1efdf92
Fernando Batista <fernandohsbatista@gmail.com>
2024-11-19 11:54:15 -0300

    datomic: (alura-ecommerce) - update value

--- datomic/ecommerce/src/ecommerce/core.clj
+++ datomic/ecommerce/src/ecommerce/core.clj
@@ -4,15 +4,24 @@
             [ecommerce.db :as db]
             [ecommerce.product :as product]))
 
+(db/delete-database)
+
 (def conn (db/open-connection))
-(pprint conn)
 
 (db/create-scheme conn)
-
-(let [macbook (product/new "Macbook M1" "macbook_m1" 6000.00M)]
-  (d/transact conn [macbook]))
 
 (def db (d/db conn))
 
 (d/q '[:find ?product
        :where [?product :product/name]] db)
+
+(let [macbook (product/new "Macbook M1" "macbook_m1" 6000.00M)]
+  (d/transact conn [macbook]))
+
+(let [iphone (product/new "Iphone 15" "/iphone_15" 7500.00M)
+      result @(d/transact conn [iphone])
+      product-id (first (vals (:tempids result)))]
+  (pprint result)
+  (pprint @(d/transact conn [[:db/add product-id :product/price 7800.00M]])))
+
+
