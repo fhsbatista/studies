9fcc2552ebb6b1551cb79918e4813622846ed859
Fernando Batista <fernandohsbatista@gmail.com>
2024-11-23 16:48:56 -0300

    datomic: (alura-ecommerce) - setting category on product creation

--- datomic/ecommerce/src/ecommerce/core.clj
+++ datomic/ecommerce/src/ecommerce/core.clj
@@ -11,23 +11,25 @@
 
 (db/create-scheme! conn)
 
-(def macbook (product/new "Macbook M1" "/macbook_m1" 6000.00M))
-(def iphone2 (product/new "Iphone 16" "/iphone_15" 7500.00M))
-(def iphone (product/new "Iphone 15" "/iphone_16" 7500.00M))
-(def watch {:product/name "Apple watch"})
-
-(db/add-products! [macbook iphone iphone2 watch])
-
-(let [mug (product/new "Black Mug" "/black_mug" 12.00M)
-      result @(d/transact conn [mug])
-      product-id (first (vals (:tempids result)))]
-  (d/transact conn [[:db/add product-id :product/price 13.00M]]))
-
 (def books (category/new "Books"))
 (def keyboards (category/new "Keyboards"))
 (def electronics (category/new "Electronics"))
 
 (db/add-categories! [books keyboards electronics])
+
+(def macbook (product/new "Macbook M1" "/macbook_m1" 6000.00M (:category/id electronics)))
+(def iphone2 (product/new "Iphone 16" "/iphone_15" 7500.00M (:category/id electronics)))
+(def iphone (product/new "Iphone 15" "/iphone_16" 7500.00M (:category/id electronics)))
+(def mxkeys (product/new "Logitech MX Keys" "/mxkeys" 7500.00M  (:category/id keyboards)))
+(def clojure-brave (product/new "Clojure for the true and brave" "/clojure_brave" 7500.00M (:category/id books)))
+(def watch {:product/name "Apple watch"})
+
+(db/add-products! [macbook iphone iphone2 watch mxkeys clojure-brave])
+
+(let [mug (product/new "Black Mug" "/black_mug" 12.00M)
+      result @(d/transact conn [mug])
+      product-id (first (vals (:tempids result)))]
+  (d/transact conn [[:db/add product-id :product/price 13.00M]]))
 
 (db/set-category-on-products! [macbook iphone iphone2] electronics)
 
@@ -41,6 +43,7 @@
 ;(pprint (db/find-by-price 7500.00M))
 ;(pprint (db/find-by-min-price 7000))
 
+(pprint (db/find-categories))
 (pprint (db/list-products-and-categories))
 (pprint (db/find-products-by-category-forward "Electronics"))
 (pprint (db/find-products-by-category-backwards "Electronics"))
--- datomic/ecommerce/src/ecommerce/product.clj
+++ datomic/ecommerce/src/ecommerce/product.clj
@@ -3,8 +3,17 @@
 (defn uuid []
   (java.util.UUID/randomUUID))
 
-(defn new [name slug price]
-  {:product/id (uuid)
-   :product/name  name
-   :product/slug  slug
-   :product/price price})
+(defn new
+  ([name slug price]
+   {
+    :product/id    (uuid)
+    :product/name  name
+    :product/slug  slug
+    :product/price price
+    })
+  ([name slug price category-id]
+   {:product/id       (uuid)
+    :product/name     name
+    :product/slug     slug
+    :product/price    price
+    :product/category [:category/id category-id]}))
