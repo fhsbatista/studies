9fde6919d3f509ce47146c436b54369cb4eaf95f
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-21 20:07:48 -0300

    go: (alura-go-1-linguagem-google) - strings concatenation and var types

--- go/alura_1/src/hello/hello.go
+++ go/alura_1/src/hello/hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var name string = "Fernando"
+	var age int = 29
+	var version float32 = 1.1
+	fmt.Println("Hello Mr.", name, "Age: ", age)
+	fmt.Println("This program version is", version)
 }
