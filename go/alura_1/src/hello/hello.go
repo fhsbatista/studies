7b217ce03efddc862d9230e9689e7db64ccb7eb6
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-21 20:10:23 -0300

    go: (alura-go-1-linguagem-google) - types inference

--- go/alura_1/src/hello/hello.go
+++ go/alura_1/src/hello/hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Fernando"
-	var age int = 29
-	var version float32 = 1.1
+	var name = "Fernando"
+	var age = 29
+	var version = 1.1
 	fmt.Println("Hello Mr.", name, "Age: ", age)
 	fmt.Println("This program version is", version)
 }
