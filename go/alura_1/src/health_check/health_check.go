c22d4901e2d6001e34b155ae9b9a85294e4b9c77
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-21 20:37:38 -0300

    go: (alura-go-1-linguagem-google) - switch/case statement

--- go/alura_1/src/health_check/health_check.go
+++ go/alura_1/src/health_check/health_check.go
@@ -20,13 +20,27 @@ func main() {
 	var command int
 	fmt.Scan(&command)
 
-	if command == 1 {
+	// if command == 1 {
+	// 	fmt.Println("Monitoring started")
+	// } else if command == 2 {
+	// 	fmt.Println("Showing logs")
+	// } else if command == 0 {
+	// 	fmt.Println("Exiting")
+	// } else {
+	// 	fmt.Println("Invalid option")
+	// }
+
+	switch command {
+	case 1:
+		// break -> "break" is not mandatory in Go. 
+		// The case used will be the first one which condition is true. 
+		// Only the first. Therefore "break" is not necessary.
 		fmt.Println("Monitoring started")
-	} else if command == 2 {
+	case 2:
 		fmt.Println("Showing logs")
-	} else if command == 0 {
+	case 0:
 		fmt.Println("Exiting")
-	} else {
+	default:
 		fmt.Println("Invalid option")
 	}
 }
