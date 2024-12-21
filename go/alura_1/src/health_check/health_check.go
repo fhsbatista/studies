26fb035953bef96fbfcd12ba8f0d086463ecdcbd
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-21 20:47:08 -0300

    go: (alura-go-1-linguagem-google) - extract code to specialized functions

--- go/alura_1/src/health_check/health_check.go
+++ go/alura_1/src/health_check/health_check.go
@@ -3,32 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	name := "Fernnado"
-	version := "1.0"
+	showIntroduction()
+	showMenu()
 
-	fmt.Println("Hello, Mr.", name)
-	fmt.Println("This program's version is", version)
-
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-
-	fmt.Println("1 - Start monitoring")
-	fmt.Println("2 - Show logs")
-	fmt.Println("0 - Exit")
-
-	var command int
-	fmt.Scan(&command)
-
-	// if command == 1 {
-	// 	fmt.Println("Monitoring started")
-	// } else if command == 2 {
-	// 	fmt.Println("Showing logs")
-	// } else if command == 0 {
-	// 	fmt.Println("Exiting")
-	// } else {
-	// 	fmt.Println("Invalid option")
-	// }
+	command := readCommand()
 
 	switch command {
 	case 1:
@@ -43,4 +21,29 @@ func main() {
 	default:
 		fmt.Println("Invalid option")
 	}
+}
+
+func showIntroduction() {
+	name := "Fernnado"
+	version := "1.0"
+
+	fmt.Println("Hello, Mr.", name)
+	fmt.Println("This program's version is", version)
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+}
+
+func showMenu() {
+	fmt.Println("1 - Start monitoring")
+	fmt.Println("2 - Show logs")
+	fmt.Println("0 - Exit")
+}
+
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
+
+	return command
 }
