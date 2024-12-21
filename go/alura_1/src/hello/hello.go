3f5831c82119b83d4d57afb6396c9631d30857b1
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-21 20:12:50 -0300

    go: (alura-go-1-linguagem-google) - checking which type a var is

--- go/alura_1/src/hello/hello.go
+++ go/alura_1/src/hello/hello.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main() {
 	var name = "Fernando"
@@ -8,4 +9,8 @@ func main() {
 	var version = 1.1
 	fmt.Println("Hello Mr.", name, "Age: ", age)
 	fmt.Println("This program version is", version)
+
+	fmt.Println("Name var type is", reflect.TypeOf(name))
+	fmt.Println("Age var type is", reflect.TypeOf(age))
+	fmt.Println("Version var type is", reflect.TypeOf(version))
 }
