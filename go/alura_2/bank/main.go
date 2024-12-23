d744d08e203e1051f8d51cafbf99b7aef784dbd1
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-23 20:45:46 -0300

    go: (alura-go-2-OOP) - transfer

--- go/alura_2/bank/main.go
+++ go/alura_2/bank/main.go
@@ -36,6 +36,23 @@ func (a *Account) Deposit(value float64) (string, float64) {
 	return "Deposit succees", a.balance
 }
 
+func (a *Account) Transfer(value float64, destination *Account) (string, float64) {
+	validValue := value > 0
+	if !validValue {
+		return "Value must be greater than 0", a.balance
+	}
+
+	hasBalance := a.balance >= value
+	if !hasBalance {
+		return "Could not transfer because there is not sufficient balance", a.balance
+	}
+
+	a.balance -= value
+	destination.balance += value
+
+	return "Transfer success", a.balance
+}
+
 func (a *Account) BalanceInfo() {
 	fmt.Println("Current balance:", a.balance)
 }
@@ -48,8 +65,21 @@ func main() {
 		balance: 0,
 	}
 
+	account2 := Account{
+		holder:  "Batista Fernando",
+		branch:  123,
+		account: 12345,
+		balance: 0,
+	}
+
 	fmt.Println(account.Deposit(0))
 	fmt.Println(account.Deposit(200))
 	fmt.Println(account.Withdraw(200))
 	fmt.Println(account.Withdraw(200))
+
+	fmt.Println(account.Deposit(200))
+	fmt.Println(account.Transfer(140, &account2))
+
+	account.BalanceInfo()
+	account2.BalanceInfo()
 }
