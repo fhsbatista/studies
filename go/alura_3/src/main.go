00ba40e84a316142446497c5edc34f1546b142ef
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-24 10:15:38 -0300

    go: (alura-go-3-web) - populate html with dynamic products

--- go/alura_3/src/main.go
+++ go/alura_3/src/main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,10 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{"T-Shirt", "Oversized", 29.0, 10},
+		{"Laptop", "Very fast", 2900.0, 10},
+		{"Sneaker", "Confortable", 290.0, 10},
+	}
+	templates.ExecuteTemplate(w, "Index", products)
 }
