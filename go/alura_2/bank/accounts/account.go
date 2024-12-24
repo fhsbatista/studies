6f4f1a01907f067506e27cb7ea4325f7688bd24f
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-23 21:02:06 -0300

    go: (alura-go-2-OOP) - extract account to specialized package

---
+++ go/alura_2/bank/accounts/account.go
@@ -0,0 +1,50 @@
+package accounts
+
+type Account struct {
+	Holder  string
+	Branch  int
+	Account int
+	Balance float64
+}
+
+func (a *Account) Withdraw(value float64) (string, float64) {
+	validValue := value > 0
+	if !validValue {
+		return "Value must be greater than 0", a.Balance
+	}
+
+	hasBalance := a.Balance >= value
+	if !hasBalance {
+		return "Could not withdraw because there is not sufficient Balance", a.Balance
+	}
+
+	a.Balance -= value
+	return "Withdraw success", a.Balance
+}
+
+func (a *Account) Deposit(value float64) (string, float64) {
+	validValue := value > 0
+	if !validValue {
+		return "Value must be greater than 0", a.Balance
+	}
+
+	a.Balance += value
+	return "Deposit succees", a.Balance
+}
+
+func (a *Account) Transfer(value float64, destination *Account) (string, float64) {
+	validValue := value > 0
+	if !validValue {
+		return "Value must be greater than 0", a.Balance
+	}
+
+	hasBalance := a.Balance >= value
+	if !hasBalance {
+		return "Could not transfer because there is not sufficient Balance", a.Balance
+	}
+
+	a.Balance -= value
+	destination.Balance += value
+
+	return "Transfer success", a.Balance
+}
--- go/alura_2/bank/main.go
+++ go/alura_2/bank/main.go
@@ -2,74 +2,26 @@ package main
 
 import (
 	"fmt"
+	"bank/accounts"
 )
 
-type Account struct {
-	holder  string
-	branch  int
-	account int
-	balance float64
-}
-
-func (a *Account) Withdraw(value float64) (string, float64) {
-	validValue := value > 0
-	if !validValue {
-		return "Value must be greater than 0", a.balance
-	}
-
-	hasBalance := a.balance >= value
-	if !hasBalance {
-		return "Could not withdraw because there is not sufficient balance", a.balance
-	}
-
-	a.balance -= value
-	return "Withdraw success", a.balance
-}
-
-func (a *Account) Deposit(value float64) (string, float64) {
-	validValue := value > 0
-	if !validValue {
-		return "Value must be greater than 0", a.balance
-	}
-
-	a.balance += value
-	return "Deposit succees", a.balance
-}
-
-func (a *Account) Transfer(value float64, destination *Account) (string, float64) {
-	validValue := value > 0
-	if !validValue {
-		return "Value must be greater than 0", a.balance
-	}
-
-	hasBalance := a.balance >= value
-	if !hasBalance {
-		return "Could not transfer because there is not sufficient balance", a.balance
-	}
-
-	a.balance -= value
-	destination.balance += value
-
-	return "Transfer success", a.balance
-}
-
-func (a *Account) BalanceInfo() {
-	fmt.Println("Current balance:", a.balance)
+func BalanceInfo(account accounts.Account) {
+	fmt.Println("Current Balance:", account.Balance)
 }
 
 func main() {
-	account := Account{
-		holder:  "Fernando Batista",
-		branch:  123,
-		account: 12345,
-		balance: 0,
+	account := accounts.Account{
+		Holder:  "Fernando Batista",
+		Branch:  123,
+		Account: 12345,
+		Balance: 0,
 	}
 
-	account2 := Account{
-		holder:  "Batista Fernando",
-		branch:  123,
-		account: 12345,
-		balance: 0,
+	account2 := accounts.Account{
+		Holder:  "Fernando Batista",
+		Branch:  124,
+		Account: 12345,
+		Balance: 0,
 	}
 
 	fmt.Println(account.Deposit(0))
@@ -80,6 +32,6 @@ func main() {
 	fmt.Println(account.Deposit(200))
 	fmt.Println(account.Transfer(140, &account2))
 
-	account.BalanceInfo()
-	account2.BalanceInfo()
+	BalanceInfo(account)
+	BalanceInfo(account2)
 }
