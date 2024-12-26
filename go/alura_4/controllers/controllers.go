2903c9524e1f56a58528735abecb265bfd7d9516
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-26 19:11:59 -0300

    go: (alura-go-4-api) - create personality api

--- go/alura_4/controllers/controllers.go
+++ go/alura_4/controllers/controllers.go
@@ -27,3 +27,11 @@ func Personality(w http.ResponseWriter, r *http.Request) {
 	database.DB.First(&personality, id)
 	json.NewEncoder(w).Encode(personality)
 }
+
+func CreatePersonality(w http.ResponseWriter, r *http.Request) {
+	var personality models.Personality
+	json.NewDecoder(r.Body).Decode(&personality)
+
+	database.DB.Create(&personality)
+	json.NewEncoder(w).Encode(personality)
+}
