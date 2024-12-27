98f10216bb76a68a5597e7756455b5ad6432e05e
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-27 08:07:40 -0300

    go: (alura-go-5-gin) - greetings api

--- go/alura_5/controllers/controllers.go
+++ go/alura_5/controllers/controllers.go
@@ -1,10 +1,20 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func Students(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"id":   "1",
 		"name": "Fernando Batista",
+	})
+}
+
+func Greetings(c *gin.Context) {
+	name := c.Params.ByName("name")
+
+	c.JSON(200, gin.H{
+		"message": "Hello, " + name + ", how are u?",
 	})
 }
--- go/alura_5/routes/routes.go
+++ go/alura_5/routes/routes.go
@@ -10,6 +10,7 @@ func HandleRequests() {
 	r := gin.Default()
 
 	r.GET("/students", controllers.Students)
+	r.GET("/greetings/:name", controllers.Greetings)
 
 	r.Run(":8000")
 }
