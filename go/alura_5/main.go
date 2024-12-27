14b7ff267217c0dac54a0d2b527e541a9ca3c04c
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-27 10:20:57 -0300

    go: (alura-go-5-gin) - setup gorm and postgres

--- go/alura_5/main.go
+++ go/alura_5/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"api-gin/database"
 	"api-gin/models"
 	"api-gin/routes"
 )
 
 func main() {
+	database.ConnectDatabase()
 	models.Students = []models.Student{
 		{Name: "Fernando Batista", CPF: "12312312312", RG: "121231232"},
 		{Name: "Fulano Batista", CPF: "12312312312", RG: "121231232"},
