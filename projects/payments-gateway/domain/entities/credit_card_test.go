b80f88344c70431574af81f9bd87c7dc0d28630f
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-30 17:32:51 -0300

    projects: (payments-gateway) - ensure credit card is created when number is valid

--- projects/payments-gateway/domain/entities/credit_card_test.go
+++ projects/payments-gateway/domain/entities/credit_card_test.go
@@ -9,4 +9,7 @@ import (
 func TestCreditCardNumber(t *testing.T) {
 	_, err := NewCreditCard("40000000000000000", "José Silveira", 12, 2024, 123)
 	assert.Equal(t, "invalid credit card number", err.Error())
+
+	_, err = NewCreditCard("4193523830170205", "José Silveira", 12, 2024, 123)
+	assert.Nil(t, err)
 }
