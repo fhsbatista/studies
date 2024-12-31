409c11a4fd7c82bfd028d2c1fb3f287f275f8b06
Fernando Batista <fernandohsbatista@gmail.com>
2024-12-31 17:30:26 -0300

    projects: (payments-gateway) - sqlite setup for tests

---
+++ projects/payments-gateway/adapters/repositories/transaction_repository_db_test.go
@@ -0,0 +1,21 @@
+package repository
+
+import (
+	"os"
+	fixture "payments-gateway/adapters/repositories/fixtures"
+	"payments-gateway/domain/entities"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransactionRepositoryDbInsert(t *testing.T) {
+	migrationsDir := os.DirFS("fixtures/sql")
+	db := fixture.Up(migrationsDir)
+	defer fixture.Down(db, migrationsDir)
+
+	repository := NewTransactionRepositoryDb(db)
+	err := repository.Insert("1", "1", 11.0, entities.APPROVED, "")
+
+	assert.Nil(t, err)
+}
