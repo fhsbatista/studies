aa6020670c763c15a48b3380b79d07858151dda9
Fernando Batista <fernandohsbatista@gmail.com>
2025-01-02 11:27:25 -0300

    spring: (alura-lambdas-streams-spring | screenmatch) - refactor moving code to specialized class

--- spring/alura_lambdas-streams-spring/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenMatchApplication.java
+++ spring/alura_lambdas-streams-spring/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenMatchApplication.java
@@ -1,21 +1,9 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.Episode;
-import br.com.alura.screenmatch.model.Season;
-import br.com.alura.screenmatch.model.Show;
-import br.com.alura.screenmatch.service.ConsumeApi;
-import br.com.alura.screenmatch.service.DataMapper;
+import br.com.alura.screenmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
@@ -26,23 +14,6 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var showJson = ConsumeApi.get("https://www.omdbapi.com/?t=the+chosen&apikey=6585022c");
-        var show = DataMapper.map(showJson, Show.class);
-
-        System.out.println(show);
-
-        var episodeJson = ConsumeApi.get("https://www.omdbapi.com/?t=the+chosen&season=1&episode=4&apikey=6585022c");
-        var episode = DataMapper.map(episodeJson, Episode.class);
-        System.out.println(episode);
-
-        var seasons = new ArrayList<Season>();
-
-        for (int i = 1; i <= show.seasonsNumber(); i++) {
-            var seasonJson = ConsumeApi.get("https://www.omdbapi.com/?t=the+chosen&season=" + i + "&apikey=6585022c");
-            var season = DataMapper.map(seasonJson, Season.class);
-            seasons.add(season);
-        }
-
-        seasons.forEach(System.out::println);
+        Main.showMenu();
     }
 }
--- spring/alura_lambdas-streams-spring/screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
+++ spring/alura_lambdas-streams-spring/screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -1,4 +1,37 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.model.Season;
+import br.com.alura.screenmatch.model.Show;
+import br.com.alura.screenmatch.service.ConsumeApi;
+import br.com.alura.screenmatch.service.DataMapper;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static final String BASEURL = "https://www.omdbapi.com/?t=";
+    private static final String API_KEY = "apikey=6585022c";
+
+    public static void showMenu() {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Type the TV Show name");
+        var name = scanner.nextLine();
+
+        var showJson = ConsumeApi.get(BASEURL + name.replace(" ", "+") + "&" + API_KEY);
+        var show = DataMapper.map(showJson, Show.class);
+
+        System.out.println(show);
+
+        var seasons = new ArrayList<Season>();
+
+        for (int i = 1; i <= show.seasonsNumber(); i++) {
+            var seasonJson = ConsumeApi.get(BASEURL + name.replace(" ", "+") + "&season=" + i + "&" + API_KEY);
+            var season = DataMapper.map(seasonJson, Season.class);
+            seasons.add(season);
+        }
+
+        seasons.forEach(System.out::println);
+    }
 }
+
