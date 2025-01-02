fde81418d366d0b9f19d6ccf7ac847a2d13fb07e
Fernando Batista <fernandohsbatista@gmail.com>
2025-01-02 13:02:52 -0300

    projects: (payments-gateway) - fix credit number tests

--- projects/payments-gateway/domain/entities/credit_card_test.go
+++ projects/payments-gateway/domain/entities/credit_card_test.go
@@ -8,21 +8,21 @@ import (
 )
 
 func TestCreditCardNumber(t *testing.T) {
-	_, err := NewCreditCard("40000000000000000", "José Silveira", 12, 2024, 123)
+	_, err := NewCreditCard("40000000000000000", "José Silveira", 12, currentYear(), 123)
 	assert.Equal(t, "invalid credit card number", err.Error())
 
-	_, err = NewCreditCard("4193523830170205", "José Silveira", 12, 2024, 123)
+	_, err = NewCreditCard("4193523830170205", "José Silveira", 12, currentYear(), 123)
 	assert.Nil(t, err)
 }
 
 func TestCreditCardExpirationMonth(t *testing.T) {
-	_, err := NewCreditCard("4193523830170205", "José Silveira", 13, 2024, 123)
+	_, err := NewCreditCard("4193523830170205", "José Silveira", 13, currentYear(), 123)
 	assert.Equal(t, "invalid expiration month", err.Error())
 
-	_, err = NewCreditCard("4193523830170205", "José Silveira", 0, 2024, 123)
+	_, err = NewCreditCard("4193523830170205", "José Silveira", 0, currentYear(), 123)
 	assert.Equal(t, "invalid expiration month", err.Error())
 
-	_, err = NewCreditCard("4193523830170205", "José Silveira", 12, 2024, 123)
+	_, err = NewCreditCard("4193523830170205", "José Silveira", 12, currentYear(), 123)
 	assert.Nil(t, err)
 }
 
@@ -31,10 +31,14 @@ func TestCreditCardExpirationYear(t *testing.T) {
 	_, err := NewCreditCard("4193523830170205", "José Silveira", 12, lastYear.Year(), 123)
 	assert.Equal(t, "invalid expiration year", err.Error())
 
-	_, err = NewCreditCard("4193523830170205", "José Silveira", 12, time.Now().Year(), 123)
+	_, err = NewCreditCard("4193523830170205", "José Silveira", 12, currentYear(), 123)
 	assert.Nil(t, err)
 
 	nextYear := time.Now().AddDate(1, 0, 0)
 	_, err = NewCreditCard("4193523830170205", "José Silveira", 12, nextYear.Year(), 123)
 	assert.Nil(t, err)
+}
+
+func currentYear() int {
+	return time.Now().Year()
 }
