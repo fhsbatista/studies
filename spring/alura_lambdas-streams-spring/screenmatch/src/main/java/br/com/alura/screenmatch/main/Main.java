85656bbff12b24c1459dabbcd18d61da3c6b2a56
Fernando Batista <fernandohsbatista@gmail.com>
2025-01-03 16:07:19 -0300

    spring: (alura-lambdas-streams-spring | screenmatch) - streams to list top best and worst episodes

--- spring/alura_lambdas-streams-spring/screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
+++ spring/alura_lambdas-streams-spring/screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -1,12 +1,13 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.model.Episode;
 import br.com.alura.screenmatch.model.Season;
 import br.com.alura.screenmatch.model.Show;
 import br.com.alura.screenmatch.service.ConsumeApi;
 import br.com.alura.screenmatch.service.DataMapper;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final String BASEURL = "https://www.omdbapi.com/?t=";
@@ -38,5 +39,30 @@ public class Main {
                 System.out.println(episode.title());
             });
         });
+
+        System.out.println("Melhores 3 episódios de todos");
+        getTopEpisodes(3, seasons).forEach(System.out::println);
+
+        System.out.println("Piores 3 episódios de todos");
+        getWorstEpisodes(3, seasons).forEach(System.out::println);
+
+    }
+
+    private static List<Episode> getTopEpisodes(Integer topNumber, List<Season> seasons) {
+        return seasons.stream()
+                .flatMap(s -> s.episodes().stream())
+                .filter(e -> !e.rating().equals("N/A"))
+                .sorted(Comparator.comparing(Episode::rating).reversed())
+                .limit(3)
+                .toList();
+    }
+
+    private static List<Episode> getWorstEpisodes(Integer number, List<Season> seasons) {
+        return seasons.stream()
+                .flatMap(s -> s.episodes().stream())
+                .filter(e -> !e.rating().equals("N/A"))
+                .sorted(Comparator.comparing(Episode::rating))
+                .limit(3)
+                .toList();
     }
 }
