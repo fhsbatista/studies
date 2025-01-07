68f05020b52e23e1283fc155bd6198ee7eb911d8
Fernando Batista <fernandohsbatista@gmail.com>
2025-01-07 10:27:02 -0300

    projects: (payments-gateway) - (backend | nestjs) - mysql setup

--- projects/backend/src/orders/entities/order.entity.ts
+++ projects/backend/src/orders/entities/order.entity.ts
@@ -22,7 +22,7 @@ export enum OrderStatus {
 export class Order extends Model {
   @PrimaryKey
   @Column({
-    type: DataType.UUIDV4,
+    type: DataType.UUID,
     defaultValue: DataType.UUIDV4,
   })
   id: string;
@@ -47,7 +47,7 @@ export class Order extends Model {
 
   @ForeignKey(() => Account)
   @Column({
-    type: DataType.UUIDV4,
+    type: DataType.UUID,
     allowNull: false,
   })
   account_id: string;
