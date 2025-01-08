329dd61ea29911b2696ba132e49ed278708720df
Fernando Batista <fernandohsbatista@gmail.com>
2025-01-08 12:30:43 -0300

    projects: (payments-gateway) - (backend | nestjs) - validations on orders/create body

--- projects/payments-gateway/nestjs/src/orders/dto/create-order.dto.ts
+++ projects/payments-gateway/nestjs/src/orders/dto/create-order.dto.ts
@@ -1,1 +1,27 @@
-export class CreateOrderDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsNotEmpty()
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  credit_card_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  credit_card_number: string;
+
+  @IsString()
+  @IsNotEmpty()
+  credit_card_expiration_month: string;
+
+  @IsString()
+  @IsNotEmpty()
+  credit_card_expiration_year: string;
+
+  @IsString()
+  @IsNotEmpty()
+  credit_card_expiration_cvv: string;
+}
