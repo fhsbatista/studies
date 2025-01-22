a0fc96664e286ac7a1f906429d5797f022158f32
Fernando Batista <fernandohsbatista@gmail.com>
2025-01-22 16:15:55 -0300

    spring: (alura-spring-rest) - handle 404 errors

---
+++ spring/alura-spring-rest/api/src/main/java/dr/center/api/infra/ErrorHandler.java
@@ -0,0 +1,15 @@
+package dr.center.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handle404() {
+            return ResponseEntity.notFound().build();
+    }
+}
