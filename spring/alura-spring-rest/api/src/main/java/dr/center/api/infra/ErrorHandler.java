d71ac98c76d9fc7ff669e32c2f67b3e31591c66b
Fernando Batista <fernandohsbatista@gmail.com>
2025-01-22 16:28:34 -0300

    spring: (alura-spring-rest) - handle 400 errors

--- spring/alura-spring-rest/api/src/main/java/dr/center/api/infra/ErrorHandler.java
+++ spring/alura-spring-rest/api/src/main/java/dr/center/api/infra/ErrorHandler.java
@@ -2,6 +2,8 @@ package dr.center.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +12,21 @@ public class ErrorHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity handle404() {
-            return ResponseEntity.notFound().build();
+        return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handle400(MethodArgumentNotValidException exception) {
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(
+                errors.stream().map(ValidationErrorData::new).toList()
+        );
+    }
+
+    private record ValidationErrorData(String field, String message) {
+        public ValidationErrorData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
 }
